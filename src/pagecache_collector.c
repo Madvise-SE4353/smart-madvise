702446bbbc885eca32b851dc9d997fb3e5dabbc6
ghazariann <vahagn.ghazayan@gmail.com>
2024-08-04 05:59:48 +0000

    solved underflow issue

--- src/pagecache_collector.c
+++ src/pagecache_collector.c
@@ -12,7 +12,7 @@
 #include "global_map.h"
 
 // change stat threshold
-#define SEQ_THRESHOLD 1000
+#define SEQ_THRESHOLD 100
 // extern pid_t target_pid_collect;
 // extern unsigned long start_address_collect;
 // extern size_t length_collect;
@@ -36,61 +36,59 @@ int handle_pre_pagefault(struct kprobe *p, struct pt_regs *regs) {
     }
 
     if (current_pid_info->tracked) {
-        u64 page_addr = address >> 12;
+        unsigned long page_addr = address >> 16;
         // if (current_pid_info->last_addr + 1 == page_addr) {
         //     current_pid_info->seq_access_count++;
         // }
+        // .. print start address and len
         if ( address >= current_pid_info->start_address_collect 
           && address < current_pid_info->start_address_collect + current_pid_info->length_collect){
+                    // printk("address: %lx, page_addr: %lx\n", address, page_addr);
             if (current_pid_info->last_addr + 1 == page_addr) {
+
                 current_pid_info->seq_access_count++;
-                // pr_info("seq counter: %d\n", current_pid_info->seq_access_count);
+                // if (current_pid_info->seq_access_count !=1)
+                //     pr_info("seq counter: %d\n", current_pid_info->seq_access_count);
             }
               else{
             current_pid_info->rand_access_count++;
-            // pr_info("rand counter: %d\n", current_pid_info->rand_access_count);
         }
             // pr_info("smart-madvise-collector: tracked process and address, pid: %d, counter: %d\n", pid, current_pid_info->seq_access_count);
             // DO STH HERE
             switch(current_pid_info->state) {
                 case SMART_MADVISE_STATE_INITIAL:
-                    if(current_pid_info->seq_access_count - current_pid_info->rand_access_count > SEQ_THRESHOLD){
+                    if(current_pid_info->seq_access_count > current_pid_info->rand_access_count + SEQ_THRESHOLD){
                         pr_info("smart-madvise: switching to MADV_SEQUENTIAL, deregistering PID %d\n", pid);
                         add_task(&task_map_global, current_pid_info->pid, current_pid_info->start_address_collect, current_pid_info->length_collect, SMART_MADVISE_TASK_MADVISE, SMART_MADVISE_SEQUENTIAL);
                         current_pid_info->state = SMART_MADVISE_STATE_SEQUENTIAL;
-                // current_pid_info->seq_access_count = 0;
-                // current_pid_info->rand_access_count = 0;
                     }
-                    else if (current_pid_info->rand_access_count - current_pid_info->seq_access_count > SEQ_THRESHOLD){
+                    else if (current_pid_info->rand_access_count > current_pid_info->seq_access_count + SEQ_THRESHOLD){
                         pr_info("smart-madvise: switching to MADV_RANDOM, deregistering PID %d\n", pid);
                         add_task(&task_map_global, current_pid_info->pid, current_pid_info->start_address_collect, current_pid_info->length_collect, SMART_MADVISE_TASK_MADVISE, SMART_MADVISE_RANDOM);
                         current_pid_info->state = SMART_MADVISE_STATE_RANDOM;
-                                        current_pid_info->seq_access_count = 0;
-                // current_pid_info->rand_access_count = 0;
-
                     }
                     break;
-            //     case SMART_MADVISE_STATE_SEQUENTIAL:
-            //            // reseting the counter to not make it too big 
-            //     // same as not tracking the state if we are in it
-            //     current_pid_info->seq_access_count = 0;
-            //         if(current_pid_info->rand_access_count - current_pid_info->seq_access_count > SEQ_THRESHOLD){
-            //             pr_info("smart-madvise: switching to MADV_RANDOM, deregistering PID %d\n", pid);
-            //             add_task(&task_map_global, current_pid_info->pid, current_pid_info->start_address_collect, current_pid_info->length_collect, SMART_MADVISE_TASK_MADVISE, SMART_MADVISE_RANDOM);
-            //             current_pid_info->state = SMART_MADVISE_STATE_RANDOM;
-            //         }
-            //         break;
-            //     case SMART_MADVISE_STATE_RANDOM:
-            //     current_pid_info->rand_access_count = 0;
-            //         if(current_pid_info->seq_access_count - current_pid_info->rand_access_count > SEQ_THRESHOLD){
-            //             pr_info("smart-madvise: switching to MADV_SEQUENTIAL, deregistering PID %d\n", pid);
-            //             add_task(&task_map_global, current_pid_info->pid, current_pid_info->start_address_collect, current_pid_info->length_collect, SMART_MADVISE_TASK_MADVISE, SMART_MADVISE_SEQUENTIAL);
-            //             current_pid_info->state = SMART_MADVISE_STATE_SEQUENTIAL;
-            //         }
-            //         break;
+                case SMART_MADVISE_STATE_SEQUENTIAL:
+                       // reseting the counter to not make it too big 
+                // same as not tracking the state if we are in it
+                current_pid_info->seq_access_count = 0;
+                if(current_pid_info->rand_access_count > current_pid_info->seq_access_count + SEQ_THRESHOLD){
+                        pr_info("smart-madvise: switching to MADV_RANDOM, deregistering PID %d\n", pid);
+                        add_task(&task_map_global, current_pid_info->pid, current_pid_info->start_address_collect, current_pid_info->length_collect, SMART_MADVISE_TASK_MADVISE, SMART_MADVISE_RANDOM);
+                        current_pid_info->state = SMART_MADVISE_STATE_RANDOM;
+                    }
+                    break;
+                case SMART_MADVISE_STATE_RANDOM:
+                current_pid_info->rand_access_count = 0;
+                    if(current_pid_info->seq_access_count > current_pid_info->rand_access_count + SEQ_THRESHOLD){
+                        pr_info("smart-madvise: switching to MADV_SEQUENTIAL, deregistering PID %d\n", pid);
+                        add_task(&task_map_global, current_pid_info->pid, current_pid_info->start_address_collect, current_pid_info->length_collect, SMART_MADVISE_TASK_MADVISE, SMART_MADVISE_SEQUENTIAL);
+                        current_pid_info->state = SMART_MADVISE_STATE_SEQUENTIAL;
+                    }
+                    break;
                 default:
                     break;
-            // }
+            }
             
         }
         pid_data[idx].last_addr = page_addr;
